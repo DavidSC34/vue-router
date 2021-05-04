b25b063c1a6946d4786c738e170990b2d092d192
DavidSC <david@wbcboxing.com>
2021-05-04 02:44:29 -0500

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <Titulo texto="Pagina About"/>
   </div>
 </template>
+<script>
+import Titulo from "../components/Titulo";
+export default {
+  name:'About',
+  components: {
+    Titulo,
+  },
+}
+</script>
---
+++ src/views/Blog.vue
@@ -0,0 +1,17 @@
+<template>
+  <Titulo texto="Titulo de mi blog"/>
+</template>
+
+<script>
+import Titulo from "../components/Titulo";
+    export default {
+        name:'Blog',
+        components: {
+            Titulo,
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
